cf712cc798339f127edecfaaff3342fe0ce292f9
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2019-01-13 19:05:34 -0600

    remove chatter and format

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -1,99 +1,79 @@
 #include <ArduinoHardware.h>
-#include <f1tenths_controller/drive_values.h>
+#include <control/drive_values.h>
 #include <ros.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Empty.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/String.h>
 
-ros::NodeHandle nh;
-
-boolean flagStop = false;
-int pwm_center_value = 9830; //  15% duty cycle
-int pwm_lowerlimit = 6554;   //  10% duty cycle
-int pwm_upperlimit = 13108;  //  20% duty cycle
-
-f1tenths_controller::drive_values prev_pwm;
-
-int servoPin = 6;
-int motorPin = 5;
-
-std_msgs::Int32 str_msg;
-ros::Publisher chatter("chatter", &str_msg);
-
-int kill_pin = 2;
+int PWM_CENTER = 9830; //  15% duty cycle
+int PWM_LOWER = 6554;   //  10% duty cycle
+int PWM_HIGHER = 13108;  //  20% duty cycle
+int SERVO_PIN = 6;
+int MOTOR_PIN = 5;
+int KILL_PIN = 2;
 unsigned long duration = 0;
+boolean emergency_stop = false;
 
-void messageDrive(const f1tenths_controller::drive_values &pwm) {
-  if (!flagStop) {
-    str_msg.data = pwm.pwm_drive;
-    chatter.publish(&str_msg);
-    
-    if ((prev_pwm.pwm_drive < pwm_center_value) != (pwm.pwm_drive < pwm_center_value)) {
-      analogWrite(motorPin, pwm_center_value);
-      // delay(1)
-    }
-    
-    prev_pwm = pwm;
-    
-    if (pwm.pwm_angle < pwm_lowerlimit) {
-      analogWrite(servoPin, pwm_lowerlimit); //  Safety lower limit
-    } else if (pwm.pwm_angle > pwm_upperlimit) {
-      analogWrite(servoPin, pwm_upperlimit); //  Safety upper limit
-    } else {
-      analogWrite(servoPin, pwm.pwm_drive); //  Incoming data
-    }
+void messageDrive(const control::drive_values &pwm);
+void messageEmergencyStop(const std_msgs::Bool &flag);
 
-    if (pwm.pwm_drive < pwm_lowerlimit) {
-      analogWrite(motorPin, pwm_lowerlimit); //  Safety lower limit
-    } else if (pwm.pwm_drive > pwm_upperlimit) {
-      analogWrite(motorPin, pwm_upperlimit); //  Safety upper limit
-    } else {
-      analogWrite(motorPin, pwm.pwm_angle); //  Incoming data
-    }
+// ROS
+ros::NodeHandle nh;
+ros::Subscriber<control::drive_values> control_serial_drive_parameters("control_serial_drive_parameters", &messageDrive);
+ros::Subscriber<std_msgs::Bool> control_emergency_stop("control_emergency_stop", &messageEmergencyStop);
+
+void messageDrive(const control::drive_values &pwm) {
+  if (emergency_stop) {
+    return;
+  }
+
+  if (pwm.pwm_angle < PWM_LOWER) {
+    analogWrite(SERVO_PIN, PWM_LOWER);
+  } else if (pwm.pwm_angle > PWM_HIGHER) {
+    analogWrite(SERVO_PIN, PWM_HIGHER);
   } else {
-    analogWrite(servoPin, pwm_center_value);
-    analogWrite(motorPin, pwm_center_value);
+    analogWrite(SERVO_PIN, pwm.pwm_drive);
+  }
+
+  if (pwm.pwm_drive < PWM_LOWER) {
+    analogWrite(MOTOR_PIN, PWM_LOWER);
+  } else if (pwm.pwm_drive > PWM_HIGHER) {
+    analogWrite(MOTOR_PIN, PWM_HIGHER);
+  } else {
+    analogWrite(MOTOR_PIN, pwm.pwm_angle);
   }
 }
 
 void messageEmergencyStop(const std_msgs::Bool &flag) {
-  flagStop = flag.data;
-  if (flagStop) {
-    analogWrite(servoPin, pwm_center_value);
-    analogWrite(motorPin, pwm_center_value);
+  emergency_stop = flag.data;
+  if (emergency_stop) {
+    analogWrite(SERVO_PIN, PWM_CENTER);
+    analogWrite(MOTOR_PIN, PWM_CENTER);
   }
 }
 
-ros::Subscriber<f1tenths_controller::drive_values> sub_drive("control_serial_drive_parameters", &messageDrive);
-ros::Subscriber<std_msgs::Bool> sub_stop("control_emergency_stop", &messageEmergencyStop);
-
 void setup() {
-  analogWriteFrequency(servoPin, 100);
-  analogWriteFrequency(motorPin, 100);
+  analogWriteFrequency(SERVO_PIN, 100);
+  analogWriteFrequency(MOTOR_PIN, 100);
   analogWriteResolution(16);
-  analogWrite(servoPin, pwm_center_value);
-  analogWrite(motorPin, pwm_center_value);
-  prev_pwm.pwm_angle = pwm_center_value;
-  prev_pwm.pwm_drive = pwm_center_value;
+  analogWrite(SERVO_PIN, PWM_CENTER);
+  analogWrite(MOTOR_PIN, PWM_CENTER);
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
   pinMode(2, INPUT);
-  //  digitalWrite(2,LOW);
 
   nh.initNode();
-  nh.subscribe(sub_drive);
-  nh.subscribe(sub_stop);
-
-  nh.advertise(chatter);
+  nh.subscribe(control_serial_drive_parameters);
+  nh.subscribe(control_emergency_stop);
 }
 
 void loop() {
   nh.spinOnce();
-  duration = pulseIn(kill_pin, HIGH, 30000);
+  duration = pulseIn(KILL_PIN, HIGH, 30000);
   while (duration > 1900) {
-    duration = pulseIn(kill_pin, HIGH, 30000);
-    analogWrite(servoPin, pwm_center_value);
-    analogWrite(motorPin, pwm_center_value);
+    duration = pulseIn(KILL_PIN, HIGH, 30000);
+    analogWrite(SERVO_PIN, PWM_CENTER);
+    analogWrite(MOTOR_PIN, PWM_CENTER);
   }
 }
